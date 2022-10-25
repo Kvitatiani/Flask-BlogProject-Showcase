3be85fe241be291093a1eb59546e2499ffad78fd
Alex <aleksikvitatiani@gmail.com>
2022-10-25 23:47:51 +0400

    Saved secret keys safely, updated database to PostgreSQL

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
